f3f59b7d4e86b15682243367d7aefd80503860de
CarolinaLZ21 <carolinalozanozuluaga@gmail.com>
2024-09-20 16:59:27 -0500

    Detallitos

--- src/gestionlibros/AdminLibros.java
+++ src/gestionlibros/AdminLibros.java
@@ -9,15 +9,15 @@ public class AdminLibros {
         Libros libroUno = new Libros("El Principito", "Antoine de Saint-Exupéry", 150, 2016);
         Libros libroDos = new Libros("El Perfume", "Patrick Süskind ", 500, 1985);
 
-        System.out.println("Datos del primer libro: " + "\n");
+        System.out.println("\n" + "Datos del primer libro: " );
         libroUno.mostraDatosLibros();
 
-        System.out.println("Datos del segundo libro: " + "\n");
+        System.out.println("\n" + "Datos del segundo libro: " );
         libroDos.mostraDatosLibros();
 
         libroUno.setPaginas(300);
         //System.out.println("Datos del primer libro modificado: " + libroUno.getPaginas());
-        System.out.println("Datos libro uno modificado: " + "\n");
+        System.out.println("\n" + "Datos libro uno modificado: ");
         libroUno.mostraDatosLibros();
 
     }
